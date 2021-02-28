3f9eb98db4218a3f92be4735699a337c544a08ec
Anna <alexia.elm@gmail.com>
2021-02-28 18:05:49 +0300

    цифры в генерации времени заезда/вызда, ценника, комнат и гостей заменены на константы

--- js/advertisements.js
+++ js/advertisements.js
@@ -1,28 +1,41 @@
 import {getRandomInteger, getRandomNumber, getRandomArrayElement, shuffleArr} from './util.js';
-import {AVATAR_INDEX_MIN, AVATAR_INDEX_MAX, TYPES, FEATURES, PHOTOS, DESCRIPTIONS, ADVERTISEMENTS_COUNT,  LATITUDE_MIN, LATITUDE_MAX, LONGITUDE_MIN, LONGITUDE_MAX, ROUNDING_DEGREE} from './advertisements-data.js';
+import {
+  AVATAR_INDEX_MIN,
+  AVATAR_INDEX_MAX,
+  TYPES,
+  FEATURES,
+  PHOTOS,
+  DESCRIPTIONS,
+  ADVERTISEMENTS_COUNT, 
+  LATITUDE_MIN,
+  LATITUDE_MAX,
+  LONGITUDE_MIN,
+  LONGITUDE_MAX,
+  ROUNDING_DEGREE,
+  MAX_ROOMS,
+  MAX_GUESTS
+} from './advertisements-data.js';
 
+const x = getRandomNumber(LATITUDE_MIN, LATITUDE_MAX, ROUNDING_DEGREE);
+const y = getRandomNumber(LONGITUDE_MIN, LONGITUDE_MAX, ROUNDING_DEGREE);
+const avatarIndex = getRandomInteger(AVATAR_INDEX_MIN, AVATAR_INDEX_MAX); 
+const address = `${x},${y}`;
+const price = getRandomInteger(1, PRICE_MAX);
+const type = getRandomArrayElement(TYPES);
+const rooms = getRandomInteger(1, MAX_ROOMS);
+const guests = getRandomInteger(1, MAX_GUESTS);
+const checkIn = `${getRandomInteger(CHECK_IN_MIN, CHECK_IN_MAX)}:00`;
+const checkOut = `${getRandomInteger(CHECK_OUT_MIN, CHECK_OUT_MAX)}:00`;
+const shuffledFeatures = shuffleArr(FEATURES);
+const features = shuffledFeatures.splice(0, getRandomInteger(1, FEATURES.length));
+const description = getRandomArrayElement(DESCRIPTIONS);
+const shuffledPhotos = shuffleArr(PHOTOS);
+const photos = shuffledPhotos.splice(0, getRandomInteger(1, PHOTOS.length));
 
 const createAdvertisement = function() {
-  const x = getRandomNumber(LATITUDE_MIN, LATITUDE_MAX, ROUNDING_DEGREE);
-  const y = getRandomNumber(LONGITUDE_MIN, LONGITUDE_MAX, ROUNDING_DEGREE);
-  
-  const avatarIndex = getRandomInteger(AVATAR_INDEX_MIN, AVATAR_INDEX_MAX);
   const author = {
     avatar: `img/avatars/user0${avatarIndex}.png`,
   }
-  
-  const address = `${x},${y}`;
-  const price = getRandomInteger(1, 100, 0)*10;
-  const type = getRandomArrayElement(TYPES);
-  const rooms = getRandomInteger(1, 10, 0);
-  const guests = getRandomInteger(1, 10, 0);
-  const checkIn = `${getRandomInteger(12, 14)}:00`;
-  const checkOut = `${getRandomInteger(12, 14)}:00`;
-  const shuffledFeatures = shuffleArr(FEATURES);
-  const features = shuffledFeatures.splice(0, getRandomInteger(1, FEATURES.length));
-  const description = getRandomArrayElement(DESCRIPTIONS);
-  const shuffledPhotos = shuffleArr(PHOTOS);
-  const photos = shuffledPhotos.splice(0, getRandomInteger(1, PHOTOS.length));
   const  offer = {
     title: 'Добро пожаловать!',
     address: address,
@@ -36,11 +49,11 @@ const createAdvertisement = function() {
     description: description,
     photos: photos,
   }
-
   const  location = {
     x: x,
     y: y,
   };
+
   const advertisement = {author, offer, location};
   return(advertisement);
 }
